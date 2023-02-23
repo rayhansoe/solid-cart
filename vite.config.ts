e2bb8da045e812fbca1a9ef0cd2cece158f36649
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-23 17:14:32 +0700

    vercel edge.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 //
-import node from "solid-start-node";
-// import vercel from "solid-start-vercel";
+// import node from "solid-start-node";
+import vercel from "solid-start-vercel";
 
 // import netlify from "solid-start-netlify";
 
 export default defineConfig(() => {
 	return {
 		plugins: [
-			solid({ ssr: true, adapter: node({ edge: false }) }),
+			// solid({ ssr: true, adapter: node({ edge: false }) }),
 			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: true }) }),
-			// solid({ ssr: true, adapter: vercel({ edge: true }) }),
+			solid({ ssr: true, adapter: vercel({ edge: true }) }),
 		],
 		ssr: { external: ["@prisma/client"] },
 	};
