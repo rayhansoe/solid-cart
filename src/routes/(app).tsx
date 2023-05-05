746fcbb816a6a4a0934cb151b0dba900b68cdbde
rayhan <rayhan.rhssoe2@gmail.com>
2023-05-05 17:25:45 +0700

    test edge.

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import autoAnimate from "@formkit/auto-animate";
 import type { VoidComponent } from "solid-js";
+import { createEffect } from "solid-js";
 import { Show } from "solid-js";
 import { Outlet, useRouteData } from "solid-start";
 import NavBar from "~/components/NavBar";
 import { getServerCartItemsData$ } from "~/services/services";
+
+export const config = {
+	runtime: "edge",
+};
 
 export function routeData() {
 	const cartItems = getServerCartItemsData$();
@@ -12,11 +19,18 @@ export function routeData() {
 
 const App: VoidComponent = () => {
 	const cartItems = useRouteData<typeof routeData>();
+	let animationParent: HTMLDivElement | ((el: HTMLDivElement) => void) | any;
+
+	createEffect(() => {
+		animationParent && autoAnimate(animationParent);
+	});
 	return (
 		<>
 			<Show when={cartItems()}>
-				<NavBar cartItems={cartItems()} />
-				<Outlet />
+				<div ref={animationParent}>
+					<NavBar cartItems={cartItems()} />
+					<Outlet />
+				</div>
 			</Show>
 		</>
 	);
